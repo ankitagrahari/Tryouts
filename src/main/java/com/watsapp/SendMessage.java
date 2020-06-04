af9532df2691198b5b89deff381d1406f4cb6a83
Ankit Agrahari <ankit.agrahari@microfocus.com>
2020-06-04 15:13:02 +0530

    Sending Watsapp Message

---
+++ src/main/java/com/watsapp/SendMessage.java
@@ -0,0 +1,26 @@
+package com.watsapp;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+
+public class SendMessage {
+
+    private String ACCOUNT_SID="ABCDEFGHIJKLMNOPQRSTVWXYZ";
+    private String AUTH_TOKEN="123abc456def789ghi";
+
+    private void sendMessageUsingTwilio(){
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        Message message = Message.creator(
+                new com.twilio.type.PhoneNumber("whatsapp:+xxxxxxxxxxx"),
+                new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
+                "Hello there!")
+                .create();
+
+        System.out.println(message.getSid());
+    }
+
+    public static void main(String[] args) {
+        SendMessage obj = new SendMessage();
+        obj.sendMessageUsingTwilio();
+    }
+}
