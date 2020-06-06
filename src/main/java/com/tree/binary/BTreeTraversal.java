9559ecef32f793cfb387de93e9c757c3722fa536
Ankit Agrahari <ankit.agrahari@microfocus.com>
2020-06-06 17:25:56 +0530

    Update BTreeTraversal.java

--- src/main/java/com/tree/binary/BTreeTraversal.java
+++ src/main/java/com/tree/binary/BTreeTraversal.java
@@ -28,4 +28,22 @@ public class BTreeTraversal {
         traverseBTreePostOrder(root.getRight());
         System.out.print(root.getValue()+"-->");
     }
+
+    public static void main(String[] args) {
+        BTreeTraversal obj = new BTreeTraversal();
+        BTree btree = new BTree();
+        TreeNode root = btree.prepareBTree();
+
+        System.out.println("Depth First Traversal - Inorder");
+        obj.traverseBTreeInOrder(root);
+
+        System.out.println();
+        System.out.println("Depth First Traversal - Preorder");
+        obj.traverseBTreePreOrder(root);
+
+        System.out.println();
+        System.out.println("Depth First Traversal - Postorder");
+        obj.traverseBTreePostOrder(root);
+
+    }
 }
