144f2556efb5731091954fc8b0c680a08f174138
Ankit Agrahari <ankit.agrahari@microfocus.com>
2020-10-17 23:47:27 +0530

    Tries Practice

---
+++ src/main/java/com/tree/trie/TrieCases.java
@@ -0,0 +1,18 @@
+package com.tree.trie;
+
+import java.util.Arrays;
+
+public class TrieCases {
+
+    public static void main(String[] args) {
+        String[] words = {"the", "there", "their", "here", "then", "therefore", "them", "can", "canned"};
+        TrieNode root = new TrieNode();
+        Arrays.asList(words).forEach(w -> {
+            System.out.println("Inserting:"+w);
+            root.insert(w);
+        });
+
+//        root.iterateTrie(root.getChildren());
+        System.out.println(root.search("cann"));
+    }
+}
---
+++ src/main/java/com/tree/trie/TrieNode.java
@@ -0,0 +1,90 @@
+package com.tree.trie;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TrieNode {
+
+    private static final int CHAR_SIZE = 26;
+    private boolean endOfWord;
+    private char value;
+    private List<TrieNode> children;
+
+    public TrieNode(){
+        //Create a new list with 26 null trienode object added
+        children = new ArrayList<>(Collections.nCopies(CHAR_SIZE, null));
+        endOfWord = false;
+    }
+
+    public TrieNode(char c){
+        //Create a new list with 26 null trienode object added
+        children = new ArrayList<>(Collections.nCopies(CHAR_SIZE, null));
+        endOfWord = false;
+        value = c;
+    }
+
+    public List<TrieNode> getChildren(){
+        return children;
+    }
+
+    public void insert(String key){
+        //Get the Object Reference from where insert will be called
+        TrieNode current = this;
+        for(Character c: key.toCharArray()){
+            if(null==current.children.get(c-'a')){
+                TrieNode childNode = new TrieNode(c);
+                current.children.set(c-'a', childNode);
+            }
+            //Next node
+            current = current.children.get(c - 'a');
+        }
+        current.endOfWord = true;
+    }
+
+    public boolean search(String key){
+        TrieNode current = this;
+
+        for(Character c: key.toCharArray()){
+            if (current.children.get(c - 'a') == null){
+                return false;
+            }
+            current = current.children.get(c-'a');
+        }
+        if(current.endOfWord)
+            return true;
+
+        return false;
+    }
+
+    public List<String> searchPartial(String key){
+        List<String> possibleMatches = new ArrayList<>();
+        TrieNode current = this;
+        StringBuilder found = new StringBuilder("");
+        for(Character c: key.toCharArray()){
+            if (current.children.get(c - 'a') != null) {
+                current = current.children.get(c - 'a');
+            }
+        }
+        found.append(key);
+        while(null!=current){
+            if (current.endOfWord)
+                possibleMatches.add(found.toString());
+            current = current;
+        }
+
+        return possibleMatches;
+    }
+
+    //TODO: Only Picking last word...check the logic.
+    public TrieNode iterateTrie(List<TrieNode> nodes){
+        for(TrieNode node: nodes){
+            if (null!=node){
+                System.out.println(node.value);
+                iterateTrie(node.children);
+                return node;
+            }
+        }
+        return null;
+    }
+}
